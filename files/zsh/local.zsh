478685260dd99819200d50f0b256c0825ee9112f
Justin Force <justin.force@gmail.com>
2013-03-22 15:45:02 -0700

    Add reloc function to reload local functions

--- files/zsh/local.zsh
+++ files/zsh/local.zsh
@@ -1,8 +1,16 @@
 ZLOCAL=$HOME/Dropbox/config/zlocal
 
-for script in \
-  global \
-  $HOST \
-; do
-  [ -e $ZLOCAL/$script ] && source $ZLOCAL/$script
-done
+##
+# Loads all local (secret/sensitive) zsh scripts. Put in a function so we can
+# reload them at will! YES!
+#
+function reloc() {
+  for script in \
+    global \
+    $HOST \
+  ; do
+    [ -e $ZLOCAL/$script ] && source $ZLOCAL/$script
+  done
+}
+
+reloc
