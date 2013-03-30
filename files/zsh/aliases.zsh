eb925fcf0451f4744f635bc92334e941851609a5
Justin Force <justin.force@gmail.com>
2013-03-30 01:09:31 -0700

    Sort aliases

--- files/zsh/aliases.zsh
+++ files/zsh/aliases.zsh
@@ -4,10 +4,10 @@ function git_aliases() {
   git config --get-regexp alias | sed 's/^alias\.//g' | awk '{printf "%7s %s %s %s %s %s %s %s %s %s\n", $1, $2, $3, $4, $5, $6, $7, $8, $9, $10}'
 }
 
+alias ack='ack-grep'
 alias g='git'
 alias gg='gitg&!'
 alias go=gnome-open
 alias grep='grep --color=auto'
 alias h='hub'
 alias ls='ls --color=auto'
-alias ack='ack-grep'
