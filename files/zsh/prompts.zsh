e818ad8c8de0635c7784c7f8238acd2c7505289f
Justin Force <justin.force@gmail.com>
2013-10-31 14:17:23 -0700

    Give typical solarized terminal a shot

--- files/zsh/prompts.zsh
+++ files/zsh/prompts.zsh
@@ -1,4 +1,4 @@
-export PROMPT='%B%F{darkcyan}%m%F{darkwhite}:%F{blue}%~%F{darkwhite}%#%b%f '
+export PROMPT='%B%F{cyan}%m%F{white}:%F{blue}%~%F{white}%#%b%f '
 source $HOME/.zsh/git-prompt/zshrc.sh
 export ZSH_THEME_GIT_PROMPT_NOCACHE=1
 export RPROMPT='$(git_super_status)'
