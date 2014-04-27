c906a64c6b9175027ed85749a9bffe9adcb19a1c
Justin Force <justin.force@gmail.com>
2014-04-26 19:28:51 -0700

    Various things from my desktop

--- files/zsh/ruby.zsh
+++ files/zsh/ruby.zsh
@@ -1,6 +1,3 @@
 for location in $HOME/.rvm /usr/local/rvm; do
   [ -e $location ] && source $location/scripts/rvm
 done
-
-# Set character encoding to UTF-8. For fuck's sake!
-export RUBYOPT="$RUBYOPT -Ku"
