5440fbe6dba0ae87f4300e2ea4bc22b3e0af4fa2
Justin Force <justin.force@gmail.com>
2014-07-29 16:54:32 -0700

    Revise nuke/kp

--- files/zsh/matt.zsh
+++ files/zsh/matt.zsh
@@ -1,27 +1,16 @@
-function nuke()
-{
-ps ax | pgrep $1 | awk '{print $1}' | xargs kill -15 $1 2> /dev/null ||
-ps ax | pgrep $1 | awk '{print $1}' | xargs kill -2 $1 2> /dev/null  ||
-ps ax | pgrep $1 | awk '{print $1}' | xargs kill -1 $1 2> /dev/null  ||
-ps ax | pgrep $1 | awk '{print $1}' | xargs kill -9 $1 2> /dev/null
-}
-
-function kp()
-{
-for proc in passenger nginx multiplexer_ctl scheduler_ctl memcached; do nuke $proc 2; done && osascript -e 'tell application "Terminal" to quit'
-echo -e "\033[31m
-                                         )  (  (    (
+KP_BANNER="\033[31m
+                                               )  (  (    (
                                          (  )  () @@  )  (( (
                                      (      (  )( @@  (  )) ) (
                                    (    (  ( ()( /---\   (()( (
-\033[34m     _______            \033[31m                )  ) )(@ !O O! )@@  ( ) ) )
-\033[34m   <   ____)            \033[31m          ) (  ( )( ()@ \ o / (@@@@@ ( ()( )
-\033[34m/--|  |(  o|            \033[31m         (  )  ) ((@@(@@ !o! @@@@(@@@@@)() (
-\033[34m|   >   \___|           \033[31m           ) ( @)@@)@  \033[36mPASSENGER\033[31m  )@@@@@()( )
-\033[34m|  /---------+           \033[31m         (@@@@)@@@( /  \033[36mNGINX\033[31m  \ @@@)@@@@@(  .
-\033[34m| |    \ =========______/|\033[31m@@@@@@@@@@@@@(@@@  \033[36mMULTIPLEXER_CTL\033[31m @(@@@(@@@ .  .
-\033[34m|  \   \\=========------\|\033[31m@@@@@@@@@@@@@@@@@  \033[36mSCHEDULER_CTL\033[31m  @@(@@)@@ @   .
-\033[34m|   \   \----+--\-))) \033[31m          @@@@@@@@@@ !!  \033[36mMEMCACHED\033[31m  !! @@)@@@ .. .
+\033[34m    ________            \033[31m                )  ) )(@ !O O! )@@  ( ) ) )
+\033[34m   /   ____)            \033[31m          ) (  ( )( ()@ \ o / (@@@@@ ( ()( )
+\033[34m+--|  | o o|            \033[31m         (  )  ) ((@@(@@ !o! @@@@(@@@@@)() (
+\033[34m|  \   \___|            \033[31m           ) ( @)@@)@  \033[36mPASSENGER\033[31m  )@@@@@()( )
+\033[34m|  /--------\            \033[31m         (@@@@)@@@( /  \033[36mNGINX\033[31m  \ @@@)@@@@@(  .
+\033[34m| |    \==========______/|\033[31m@@@@@@@@@@@@@(@@@  \033[36mMULTIPLEXER_CTL\033[31m @(@@@(@@@ .  .
+\033[34m|  \    \\=========------\|\033[31m@@@@@@@@@@@@@@@@@  \033[36mSCHEDULER_CTL\033[31m  @@(@@)@@ @   .
+\033[34m|   \    \----+--\-))) \033[31m          @@@@@@@@@@ !!  \033[36mMEMCACHED\033[31m  !! @@)@@@ .. .
 \033[34m|   |\______|_)))/   \033[31m          .    @@@@@@ !!  \033[36mTERMINAL\033[31m  !! @@(@@@ @ . .
 \033[34m \__==========      \033[31m     *        .    @@ /MM  /\O   O/\  MM\ @@@@@@@. .
 \033[34m    |   |-\   \    \033[31m      (       .      @ !!!  !! \-/ !!  !!! @@@@@ .
@@ -32,6 +21,37 @@ echo -e "\033[31m
 \033[34m    |   |_<   /   \033[31m( ) ( (  ) )   (( )  )).) ((/ |  (  | \(  )) ((. ).
 \033[34m____<_____\\__\__\033[31m(___)_))_((_(____))__(_(___.oooO_____Oooo.(_(_)_)((_
 "
+
+function nuke() {
+  (
+    for signal in TERM INT KILL; do
+      pid=$(pgrep $1)
+      if [ -z "$pid" ]; then
+        return
+      fi
+      echo "$pid" | while read pid; do
+        kill -$signal $pid
+        if [ $? = 0 ]; then
+          echo "Killed $1 with pid $pid using signal $signal"
+          return
+        else
+          sleep 10
+        fi
+      done
+    done
+  ) &
+}
+
+function kp() {
+  echo -e $KP_BANNER
+  for proc in memcached       \
+              multiplexer_ctl \
+              nginx           \
+              passenger       \
+              scheduler_ctl   \
+              Terminal        ; do
+    nuke $proc
+  done
 }
 
 function retag() {
