ae1cdaec2280473f3a286d01eb925414ef0c323d
Justin Force <justin.force@gmail.com>
2015-02-02 16:09:48 -0800

    Fix autojump.zsh for new version

--- files/zsh/autojump.zsh
+++ files/zsh/autojump.zsh
@@ -1,3 +1,8 @@
-for file in /usr/share/autojump/autojump.sh /usr/local/etc/autojump.zsh; do
-  [ -e $file ] && source $file
+#!/bin/zsh
+
+for file in /usr/share/autojump/autojump.sh /usr/local/etc/autojump.zsh /usr/local/etc/autojump.sh; do
+  if [ -e $file ]; then
+    source $file
+    return
+  fi
 done
