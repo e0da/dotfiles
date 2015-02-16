8a643c54b105014ef66ad2986e4404565cdd0048
Justin Force <justin.force@gmail.com>
2015-02-15 19:51:52 -0800

    Fix PROMPT colors

--- files/zsh/prompts.zsh
+++ files/zsh/prompts.zsh
@@ -1,4 +1,6 @@
-export PROMPT='%B%F{cyan}%m%F{white}:%F{blue}%~%F{white}%#%b%f '
+#!/usr/bin/zsh
+
+export PROMPT='%F{cyan}%m%F{white}:%F{blue}%~%F{white}%#%b%f '
 source "$HOME/.zsh/git-prompt/zshrc.sh"
 export ZSH_THEME_GIT_PROMPT_NOCACHE=1
 
