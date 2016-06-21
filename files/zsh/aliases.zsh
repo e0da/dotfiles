db88c1f3bbd69790445ae13bc006dc6f33c92fb2
Justin Force <justin.force@gmail.com>
2016-06-21 15:53:05 -0700

    Awesomeize shell aliases

--- files/zsh/aliases.zsh
+++ files/zsh/aliases.zsh
@@ -1,26 +1,17 @@
 alias ack='ack-grep'
 alias brake='bundle exec rake'
-alias d='sudo docker'
 alias g='git'
-alias gg='gitg&!'
-alias go=gnome-open
-alias grake='rake -g'
 alias grep='grep --color=auto'
-alias h='hub'
-alias pr='hub pull-request'
-alias v='vi'
-alias vd='vagrant destroy'
-alias vh='vagrant halt'
-alias vi='vim'
-alias vp='vagrant provision'
-alias vs='vagrant ssh'
-alias vu='vagrant up'
+alias t='test_launcher find'
 
-ls --color &>/dev/null
+# NeoVim a lot
+for vi in v vi vim mvim gvim; do
+  eval "alias $vi=nvim"
+done
+
+ls / --color &>/dev/null
 if [ $? = 0 ]; then
   alias ls='ls --color=auto'
 else
   alias ls='ls -G'
 fi
-
-alias t='test_launcher find'
