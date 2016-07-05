b6b1f48e1d9df1655618f58bd04ff765dbc51fbb
Justin Force <justin.force@gmail.com>
2016-07-04 22:56:01 -0700

    Make it sudo pip install neovim

--- ubuntu/install/neovim.bash
+++ ubuntu/install/neovim.bash
@@ -12,7 +12,7 @@ sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt update
 sudo apt install --yes neovim
 sudo apt install --yes python-dev python-pip python3-dev python3-pip
-pip install neovim
+sudo pip install neovim
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
 sudo update-alternatives --config vi
 sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
