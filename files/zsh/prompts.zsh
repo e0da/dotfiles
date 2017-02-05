d0a4524da709c93aa6ea42d41240b8e45aef83db
Justin Force <justin.force@gmail.com>
2017-02-04 22:39:37 -0800

    Only do haskell gitstatus if it exists

--- files/zsh/prompts.zsh
+++ files/zsh/prompts.zsh
@@ -3,7 +3,7 @@
 export PROMPT='%F{cyan}%m%F{white}:%F{blue}%~%F{white}%#%b%f '
 
 source "$HOME/.zsh/git-prompt/zshrc.sh"
-export GIT_PROMPT_EXECUTABLE="haskell"
+[ -x "$HOME/.bin/gitstatus" ] && export GIT_PROMPT_EXECUTABLE="haskell"
 
 # shellcheck disable=SC2016
 export RPROMPT='$(git_super_status)'
