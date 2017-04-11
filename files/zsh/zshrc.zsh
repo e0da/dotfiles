eb7c8add3d20f690c120741d1f196854fa9f9453
Justin Force <justin.force@gmail.com>
2017-04-11 12:11:09 -0700

    Improve RVM load reliability

--- files/zsh/zshrc.zsh
+++ files/zsh/zshrc.zsh
@@ -14,4 +14,10 @@ done
 
 export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 
+# Load RVM as a shell function unless it's already loaded
+type rvm | grep -v 'shell function' >/dev/null
+if [[ 0 = $? && -f $HOME/.rvm/scripts/rvm ]]; then
+  source $HOME/.rvm/scripts/rvm
+fi
+
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
