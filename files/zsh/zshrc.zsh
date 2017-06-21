1e756494de1cf019ec72b4dc2db2d64d9f66d111
Justin Force <justin.force@gmail.com>
2017-06-21 15:34:28 -0700

    Support rbenv

--- files/zsh/zshrc.zsh
+++ files/zsh/zshrc.zsh
@@ -1,11 +1,8 @@
-for script in  \
-  environment  \
-  prompts      \
-  aliases      \
-  autojump     \
-  functions    \
-  fzf          \
-  local        \
-; do
+# Order is significant. local should be almost last, and rbenv should be last.
+scripts=(environment prompts aliases autojump functions fzf local rbenv)
+
+for script in "${scripts[@]}"; do
   source "$HOME/.zsh/$script.zsh"
 done
+
+# vi: set ft=sh:
