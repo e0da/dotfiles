51d0f26ec14dc82843678458762b8b60c7d47185
Justin Force <justin.force@gmail.com>
2017-06-26 11:11:32 -0700

    Stop fighting fzf over .zshrc

--- files/zsh/zshrc.zsh
+++ files/zsh/zshrc.zsh
@@ -1,8 +1,11 @@
 # Order is significant. local should be almost last, and rbenv should be last.
-scripts=(environment prompts aliases autojump functions fzf local rbenv)
+scripts=(environment prompts aliases autojump functions local rbenv)
 
 for script in "${scripts[@]}"; do
   source "$HOME/.zsh/$script.zsh"
 done
+
+# fzf won't stop doing this, so I guess I'll stop fighting it.
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 # vi: set ft=sh:
