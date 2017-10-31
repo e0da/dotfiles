34ec411c8229b9c924ce19d4da3edd7dcb220458
Justin Force <justin.force@gmail.com>
2017-10-30 17:35:57 -0700

    Fix GIT_PROMPT_EXECUTABLE bin check

--- files/zsh/prompts.zsh
+++ files/zsh/prompts.zsh
@@ -3,7 +3,7 @@
 export PROMPT='%B%F{cyan}%m%F{white}:%F{blue}%~%F{white}%#%b%f '
 
 source "$HOME/.zsh/git-prompt/zshrc.sh"
-[ -x "$HOME/.bin/gitstatus" ] && export GIT_PROMPT_EXECUTABLE="haskell"
+[ -x "$HOME/.zsh/git-prompt/src/.bin/gitstatus" ] && export GIT_PROMPT_EXECUTABLE="haskell"
 
 # shellcheck disable=SC2016
 export RPROMPT='$(git_super_status)'
