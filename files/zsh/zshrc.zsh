98bc1c19853606d2c96fd1cf16ebf82cc671384f
Justin Force <justin.force@gmail.com>
2018-01-22 15:09:12 -0800

    Add nvm I guess. JavaScript sucks so fucking bad.

--- files/zsh/zshrc.zsh
+++ files/zsh/zshrc.zsh
@@ -9,3 +9,7 @@ done
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 # vi: set ft=sh:
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
