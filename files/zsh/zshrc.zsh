306da04d6f1168a5114f2a42452a156d86f331e3
Justin Force <justin.force@gmail.com>
2018-05-30 13:18:15 -0700

    Remove RVM again

--- files/zsh/zshrc.zsh
+++ files/zsh/zshrc.zsh
@@ -13,6 +13,3 @@ done
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
-export PATH="$PATH:$HOME/.rvm/bin"
