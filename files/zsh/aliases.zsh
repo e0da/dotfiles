b6220651f6382f8e747aa7a19b4232b64ac6afb9
Justin Force <justin.force@gmail.com>
2019-06-01 18:33:46 -0700

    Remove Mac ls alias

--- files/zsh/aliases.zsh
+++ files/zsh/aliases.zsh
@@ -1,6 +1,7 @@
 alias g='git'
 alias grep='grep --color=auto'
 alias here='xdg-open . 2>/dev/null'
+alias ls='ls --color=auto'
 
 for bin in pry rails rake spring rspec zat sidekiq; do
   alias $bin="best-ruby-bin ${bin}"
@@ -10,13 +11,6 @@ done
 for vi in v vi vim mvim gvim; do
   eval "alias $vi=nvim"
 done
-
-ls / --color &>/dev/null
-if [ $? = 0 ]; then
-  alias ls='ls --color=auto'
-else
-  alias ls='ls -G'
-fi
 
 # work
 alias bkcl='xdg-open "https://github.com/TrackRbyPhoneHalo/TrackR/blob/master/CHANGELOG.md"'
